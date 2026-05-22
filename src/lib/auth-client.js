6c04eca2e6afa75bb5890834a40a293170a4cd8d
asrahat <asrahat4742@gmail.com>
2026-05-22 16:36:35 +0600

    jwt token added

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,11 +1,11 @@
 import { createAuthClient } from "better-auth/react"
-// import { jwtClient } from "better-auth/client/plugins"
+import { jwtClient } from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
     baseURL: process.env.BETTER_AUTH_URL || process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
-  //     plugins: [
-  //   jwtClient() 
-  // ]
+      plugins: [
+    jwtClient() 
+  ]
 })
 export const { signIn, signUp, useSession } = createAuthClient()
